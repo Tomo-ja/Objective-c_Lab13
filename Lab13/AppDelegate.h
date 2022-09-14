bcf5b88ed2d40de3d4cc7893ce9cb243310fc16c
Tomo-ja <tomonao.hj@gmail.com>
2022-09-14 09:31:31 -0700

    Initial Commit

---
+++ Lab13/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Lab13
+//
+//  Created by Tomonao Hashiguchi on 2022-09-14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
